186ca384b0bdefa4fe8bf541cefc8523e983e9fa
Naichin <yonaichin@gmail.com>
2014-12-22 13:31:28 +0800

    update allow-cross-origin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
+  before_filter :set_cors_header
   protect_from_forgery with: :null_session
+  # For all responses in this controller, return the CORS access control headers.
   def set_cors_header
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
