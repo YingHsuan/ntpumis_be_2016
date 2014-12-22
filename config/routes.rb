28b0ff41438137c89432b8fa29bead5b1b304008
Naichin <yonaichin@gmail.com>
2014-12-22 11:49:34 +0800

    update routes teacher#create

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   # teacher routes
   post 'teacher/list' => 'teacher#list'
-  post 'teacher/list/create' => 'teacher#create'
+  post 'teacher/create' => 'teacher#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
