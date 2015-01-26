c7ca219c62c667b81ae96a9405119dc64355baee
Naichin <yonaichin@gmail.com>
2015-01-27 05:29:50 +0800

    make seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+#
+puts '==================Generate Mock Datas for Teachers=================='
+create_teachers = for i in 1..10 do
+                    Teacher.create!([name_c:"老師 #{i}",name_e:"Teacher #{i}",office_c:"辦公室 #{i}",office_e:"Office #{i}",domain_c:"專業知識 #{i}",domain_e:"Domain #{i}",degree_c:"台北大學#{i}所博士",degree_e:"Ph.d in #{i} institute ,NTPU",image_url:"http://image#{i}.jpg",title_priority:"1",is_chair:"false",email:"test#{i}@test.com",tel:"2222333#{i}",extension:"6689#{i}",employ_type:"1"])
+                  end
