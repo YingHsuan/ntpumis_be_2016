b24d55537e5bc445274f85d0f6985b2dd69e2989
Naichin <yonaichin@gmail.com>
2015-01-27 12:28:00 +0800

    update action that needs to be signed in

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,5 +1,5 @@
 class TeachersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:new, :index, :show, :edit, :create, :update, :destroy]
   require 'setting'
 
   def index
