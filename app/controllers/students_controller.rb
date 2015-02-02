e6c5b90c9c130b8a5630743fa8ad7787ac915e9c
Naichin <yonaichin@gmail.com>
2015-02-03 02:13:30 +0800

    add students_controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,14 @@
 class StudentsController < ApplicationController
+  def index
+    @students = Student.all
+  end
+
+
+
+
+
+  private
+  def student_params
+    params.require(:student).permit(:stu_no,:stu_name,:grade,:gender,:address,:tel,:mobile,:email,:supervisor1,:supervisor2,:is_graduated,:job_organization,:job_title,:job_industry)
+  end
 end
